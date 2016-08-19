8c8a8879ccfc6973dba8454f8f4fa80fc1fabe0a
Sbingo <745677058@qq.com>
2016-08-19 18:48:48 +0800

    snackbar begin

---
+++ remiderbox/src/main/java/Snackbar/CustomSnackbar.java
@@ -0,0 +1,12 @@
+package Snackbar;
+
+import android.support.design.widget.Snackbar;
+
+/**
+ * Created by Sbingo on 2016/8/19.
+ */
+public class CustomSnackbar  {
+
+    private Snackbar snackbar;
+
+}
