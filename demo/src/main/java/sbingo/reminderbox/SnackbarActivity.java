f079e0cdaa93e0238f73ca809cdc0e6586fa84fc
Sbingo <745677058@qq.com>
2016-08-26 14:58:26 +0800

    Snackbar compeleted v2.0

--- demo/src/main/java/sbingo/reminderbox/SnackbarActivity.java
+++ demo/src/main/java/sbingo/reminderbox/SnackbarActivity.java
@@ -1,13 +1,15 @@
 package sbingo.reminderbox;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import Snackbar.CustomSnackbar;
@@ -19,23 +21,22 @@ import butterknife.ButterKnife;
  */
 public class SnackbarActivity extends BaseActivity {
 
-    @BindView(R.id.tv_content)
-    TextView tvContent;
+
+    @BindView(R.id.coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
 
     private ActionBar actionBar;
-    Snackbar snackbar;
+    private Snackbar snackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO: 2016/8/23 设置布局
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.snackbar_layout);
         ButterKnife.bind(this);
+
         actionBar = getSupportActionBar();
         actionBar.setTitle("Snackbar");
         actionBar.setDisplayHomeAsUpEnabled(true);
-
-        ButterKnife.bind(this);
     }
 
     @Override
@@ -48,27 +49,68 @@ public class SnackbarActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.standard:
-                CustomSnackbar.getInstance(createSnackbar(tvContent, "不小心点错了？", "取消"))
-                        .setBackgroundColor(Color.BLUE)
-                        .addView(R.layout.activity_main,0)
-                        .show();
+                createSnackbar(coordinatorLayout, "standard", "cancel").show();
                 return true;
             case R.id.custom:
-                Toast.makeText(this, "custom Snackbar", Toast.LENGTH_SHORT).show();
+                /** use the {@link CustomSnackbar} here */
+                CustomSnackbar.getInstance(createSnackbar(coordinatorLayout, "custom", "cancel"))
+                        .setBackgroundColor(Color.GRAY)
+                        .setMessageColor(Color.GREEN)
+                        .addView(R.layout.snackbar_add_view, 0)
+                        .show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    Snackbar createSnackbar(View v, String msg, String action) {
-       return snackbar = Snackbar
+    /**
+     * create a {@link Snackbar}.
+     * Notice: to use the {@link CustomSnackbar}, pls create a {@link Snackbar} first.
+     */
+    private Snackbar createSnackbar(View v, final String msg, String action) {
+        return snackbar = Snackbar
                 .make(v, msg, Snackbar.LENGTH_LONG)
                 .setAction(action, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         snackbar.dismiss();
+                        Toast.makeText(SnackbarActivity.this, msg + "_cancel", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setActionTextColor(Color.RED)
+                .setCallback(new Snackbar.Callback() {
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        /** do sth when the Snackbar is dismissed*/
+                        switch (event) {
+                            case DISMISS_EVENT_SWIPE:
+                                /** while using the {@link CoordinatorLayout} in layout, the Snackbar can be swiped */
+                                Toast.makeText(SnackbarActivity.this, msg + "_swipe", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DISMISS_EVENT_ACTION:
+                                Toast.makeText(SnackbarActivity.this, msg + "_action", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DISMISS_EVENT_TIMEOUT:
+                                Toast.makeText(SnackbarActivity.this, msg + "_timeout", Toast.LENGTH_SHORT).show();
+                                /** you may want to do sth here */
+                                break;
+                            case DISMISS_EVENT_MANUAL:
+                                Toast.makeText(SnackbarActivity.this, msg + "_manual", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DISMISS_EVENT_CONSECUTIVE:
+                                Toast.makeText(SnackbarActivity.this, msg + "_consecutive", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                        }
+                    }
+
+                    @Override
+                    public void onShown(Snackbar snackbar) {
+                        /** do sth when the Snackbar is shown*/
+                        Toast.makeText(SnackbarActivity.this, String.format("%s_show", msg), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
+
 }
--- remiderbox/src/main/java/Snackbar/CustomSnackbar.java
+++ remiderbox/src/main/java/Snackbar/CustomSnackbar.java
@@ -4,28 +4,32 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import sbingo.remiderbox.R;
 
 /**
  * Created by Sbingo on 2016/8/19.
+ * Use this class to custom a {@link Snackbar}, such as it's background, message color,etc.
+ * Before use the {@link CustomSnackbar}, a standard Snackbar is required.
+ * Just as Snackbar, you can use method chaining to create and show a Snackbar in one statement.
  */
 public class CustomSnackbar {
 
     private static Snackbar snackbar;
     private static CustomSnackbar customSnackbar;
-    private String msg;
-    private View v;
-    private String action;
-    private int actionTextColor;
-    private Snackbar.Callback callback;
 
-
+    /**
+     * get the instance of {@link CustomSnackbar}.
+     *
+     * @param snackbar a snackbar have been created, it is required.
+     * @return
+     */
     public static CustomSnackbar getInstance(@NonNull Snackbar snackbar) {
         if (customSnackbar == null) {
             customSnackbar = new CustomSnackbar();
@@ -34,25 +38,59 @@ public class CustomSnackbar {
         return customSnackbar;
     }
 
+    /**
+     * set the {@link CustomSnackbar}'s background color
+     *
+     * @param color the color used as the background
+     * @return
+     */
     public CustomSnackbar setBackgroundColor(@ColorInt int color) {
         getBackground().setBackgroundColor(color);
         return this;
     }
 
-    public CustomSnackbar setBackgroundColor(Drawable background) {
-        getBackground().setBackground(background);
+    /**
+     * set the {@link CustomSnackbar}'s background
+     *
+     * @param drawable the drawable used as the background
+     * @return
+     */
+    public CustomSnackbar setBackground(@DrawableRes Drawable drawable) {
+        getBackground().setBackground(drawable);
         return this;
     }
 
+    /**
+     * set the message's color
+     *
+     * @param color the color to be set
+     * @return
+     */
+    public CustomSnackbar setMessageColor(@ColorInt int color) {
+        ((TextView) (getBackground().findViewById(R.id.snackbar_text))).setTextColor(color);
+        return this;
+    }
+
+    /**
+     * add a view to the {@link CustomSnackbar}.
+     * Notice: This method is not recommended, especially the layout is complicated.
+     *
+     * @param layoutId the resource id of the layout.
+     * @param index    the position at which to add the new view or -1 to add last
+     * @return
+     */
     public CustomSnackbar addView(int layoutId, int index) {
         Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) getBackground();
-        View add_view = LayoutInflater.from(getBackground().getContext()).inflate(layoutId, null);
+        View addView = LayoutInflater.from(getBackground().getContext()).inflate(layoutId, null);
         LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         p.gravity = Gravity.CENTER_VERTICAL;
-        snackbarLayout.addView(add_view, index, p);
+        snackbarLayout.addView(addView, index, p);
         return this;
     }
 
+    /**
+     * show the {@link CustomSnackbar}.
+     */
     public void show() {
         snackbar.show();
     }
